b8cbd03c9e61075a4c9018c2595ae7e8e1f8d7b0
zachary <syyun@snu.ac.kr>
2020-02-19 01:03:30 +0900

    Add Navigation Header

---
+++ src/stories/Layouts/NavHeader.stories.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react';
+import classNames from 'classnames';
+import { Logo } from '../Assets/Logo.stories';
+
+export default {
+    title: 'Layouts/NavHeader',
+};
+
+type NavigationHeaderProps = {
+    height?: number,
+    bgColor?: string,
+    bbColor?: string,
+    logo?: React.ReactNode
+}
+
+export const NavigationHeader: FunctionComponent<NavigationHeaderProps> = ({
+    height = 55,
+    bgColor = "yellow",
+    bbColor = "black",
+    logo = null,
+}) => {
+    const nav_cx = classNames(
+        `z-4 top-0 bb w-100 relative bg-${bgColor} b--${bbColor}`,
+    );
+    const div_cx = classNames('w-100 ph4-l center flex justify-between items-center pv2');
+    return (
+        <nav className={nav_cx}>
+            <div className={div_cx} style={{ height }}>
+                {logo}
+            </div>
+        </nav>
+    );
+};
+
+export const NavigationHeaderWithLogo = () => NavigationHeader({ height: 55, logo: Logo({ textColor: "blue" }) });
