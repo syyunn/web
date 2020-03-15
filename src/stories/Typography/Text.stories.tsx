e7de16fdda9105bdf004ea67015071ae1d9c3166
zachary <syyun@snu.ac.kr>
2020-03-16 02:25:39 +0900

    Add Heading

---
+++ src/stories/Typography/Text.stories.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+
+export default {
+    title: 'Typography/Text',
+};
+
+type HeadingProp = {
+    // textColor?: string
+    text?: string
+}
+
+export const Heading: FunctionComponent<HeadingProp> = ({ text = "Level 1 Heading" }) => {
+    return (
+        <h1 className="f2 lh-title navy avenir">
+            {text}
+        </h1>
+    )
+};
